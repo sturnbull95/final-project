0535fe3e78474ba8d1ba243e20725961099df228
Steven Turnbull <sturnbull95@Stevens-MacBook-Pro.local>
2017-04-05 19:42:55 -0400

    commiting first form

--- app.js
+++ app.js
@@ -1,44 +1,97 @@
+var fs = require('fs');
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var hbs = require('hbs');
+var expressSess = require("express-session");
+//var sleep = require('sleep');
+require('./db');
+var mongoose = require('mongoose');
+const URLSlugs = require('mongoose-url-slugs');
+var bodyParser = require("body-parser");
 var app = express();
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var Schema = mongoose.Schema;
 
-app.use('/', index);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+const Lists = mongoose.model('Lists');
+const User = mongoose.model('User');
+var dbResults = false;
+
+app.get('/', function(req, res) {
+  Lists.find(function(err, linkObj, count) {
+    res.render('index',{routines:linkObj,css_file:"/base.css"});
+  });
+});
+app.get('/routine',function(req,res){
+  Lists.find(function(err, linkObj, count) {
+  res.render('routine',{routines:linkObj,css_file:"/base.css"});
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+Lists.find({},function(err, linkObj, count) {
+  console.log("find callback");
+  dbResults = linkObj;
+  for(itr in dbResults){
+    let obj = dbResults[itr];
+    console.log(obj.slug);
+    app.get('/' + obj.slug, function(req,res){
+      Lists.find({'slug':obj.slug},function(err,linkObj,count){
+        let thisObj = linkObj[0];
+        console.log(thisObj);
+        res.render('routine',{routine:thisObj,css_file:"/base.css"});
+      });
+    });
+  }
 });
 
-module.exports = app;
+
+app.post('/addWorkout', function(req,res) {
+  var slug = req.body.slug;
+  console.log(slug);
+  Lists.find({'slug': slug},function(err, linkObj, count){
+    var obj = linkObj[0];
+    console.log("object");
+    console.log(obj);
+    const routine = new Lists({
+      name:req.body.name,
+      workout:req.body.workout
+    });
+    Lists.update({_id: obj._id},{$addToSet:{workouts:workout}},function(err, doc) {
+      if(!err) {
+        res.redirect('/'+ obj.slug);
+      } else {
+        res.send(err.errors.title.message);
+      }
+    });
+
+  });
+});
+
+
+
+app.post('/addRoutine', function(req,res) {
+  const routine = new Lists({
+    name:req.body.name,
+    workout:[req.body.workout]
+  });
+  routine.save(function(err, doc) {
+    if(!err) {
+      app.get('/' + doc.slug, function(req,res){
+        Lists.find({'slug':doc.slug},function(err,linkObj,count){
+          let thisObj = linkObj[0];
+          console.log(thisObj);
+          res.render('workout',{routine:thisObj,css_file:"/base.css"});
+        });
+      });
+      res.redirect('/');
+    } else {
+      res.send(err.errors.title.message);
+    }
+  });
+});
+
+app.listen(3000);
--- db.js
+++ db.js
@@ -1,12 +1,22 @@
 //initial schema
 
-var user = new mongoose.Schema{
+const mongoose = require('mongoose');
+const URLSlugs = require('mongoose-url-slugs');
+
+// schema tells mongoose the types that are allowed in a collection
+
+var Lists = new mongoose.Schema({
+	name: String,
+	workout: [String]
+});
+var User = new mongoose.Schema({
   username: String,
   hash: String,
   lists: [Lists]
-}
+});
 
-var Lists = new mongoose.Schema{
-	name: String,
-	length: Number
-	}
+Lists.plugin(URLSlugs('name'));
+mongoose.model('Lists', Lists);
+mongoose.model('User', User);
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/hw05');
