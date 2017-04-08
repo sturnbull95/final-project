ad13ae1bfe1ef1b6bc24b33e52c9e2777d7db235
Steven Turnbull <sturnbull95@Stevens-MacBook-Pro.local>
2017-04-08 13:04:24 -0400

    i dont know why i cant add on linserv2 server

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+// a taste of login! - express app demonstrating registration and login
+// express static setup
+
 var fs = require('fs');
 var express = require('express');
 var hbs = require('hbs');
 var expressSess = require("express-session");
-//var sleep = require('sleep');
 require('./db');
+var passport = require("passport");
 var mongoose = require('mongoose');
 const URLSlugs = require('mongoose-url-slugs');
 var bodyParser = require("body-parser");
@@ -14,84 +17,152 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+var RedisStore = require('connect-redis')(expressSess);
+
+module.exports = function Sessions(url, secret) {
+  var store = new RedisStore({ url: url });
+  var session = expressSession({
+    secret: secret,
+    store: store,
+    resave: true,
+    saveUninitialized: true
+  });
+
+  return session;
+};
+
 var Schema = mongoose.Schema;
-
-
-const Lists = mongoose.model('Lists');
-const User = mongoose.model('User');
-var dbResults = false;
-
-app.get('/', function(req, res) {
-  Lists.find(function(err, linkObj, count) {
-    res.render('index',{routines:linkObj,css_file:"/base.css"});
-  });
-});
-app.get('/routine',function(req,res){
-  Lists.find(function(err, linkObj, count) {
-  res.render('routine',{routines:linkObj,css_file:"/base.css"});
-  });
-});
-
-Lists.find({},function(err, linkObj, count) {
-  console.log("find callback");
-  dbResults = linkObj;
-  for(itr in dbResults){
-    let obj = dbResults[itr];
-    console.log(obj.slug);
-    app.get('/' + obj.slug, function(req,res){
-      Lists.find({'slug':obj.slug},function(err,linkObj,count){
-        let thisObj = linkObj[0];
-        console.log(thisObj);
-        res.render('routine',{routine:thisObj,css_file:"/base.css"});
-      });
+const LocalStrategy = require('passport-local').Strategy
+var session = {};
+var User = mongoose.model('User');
+var Lists = mongoose.model('Lists');
+var bcrypt = require('bcrypt');
+passport.serializeUser(function(user, done) {
+        done(null, user.id);
     });
+
+passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+app.use(passport.initialize());
+app.use(passport.session());
+const sessionOptions = {
+	secret: 'secret cookie thang (store this elsewhere!)',
+	resave: true,
+	saveUninitialized: true
+};
+app.use(expressSess(sessionOptions));
+
+app.get('/restricted',function(req,res){
+  if(!req.session.username){
+    res.redirect('login');
+  }
+  else{
+    res.render('redirect',{css_file:"/base.css"});
   }
 });
 
+app.get('/logout',function(req,res,next){
+  req.session.destroy();
+  res.redirect('/');
+});
 
-app.post('/addWorkout', function(req,res) {
-  var slug = req.body.slug;
-  console.log(slug);
-  Lists.find({'slug': slug},function(err, linkObj, count){
-    var obj = linkObj[0];
-    console.log("object");
-    console.log(obj);
-    const routine = new Lists({
-      name:req.body.name,
-      workout:req.body.workout
-    });
-    Lists.update({_id: obj._id},{$addToSet:{workouts:workout}},function(err, doc) {
-      if(!err) {
-        res.redirect('/'+ obj.slug);
+  app.post('/login', function(req, res,next) {
+      User.findOne({'username':req.body.username},function(err, user,count) {
+      if(!err && user) {
+          if(bcrypt.hashSync(req.body.password,user.pwSalt) == user.password){
+            req.session.regenerate((err) => {
+              if (!err) {
+                req.session.username = user.username;
+                req.session.password = user.password;
+              } else {
+                console.log('error');
+                res.send('an error occurred, please see the server logs for more information');
+              }
+            });
+            req.logIn(user, function(err) {
+              res.render('index',{css_file:"/base.css",username:user.username});
+            });
+          }
+          else{
+            res.render('login', {message:'Your login or password is incorrect.',css_file:"/base.css"});
+          }
       } else {
-        res.send(err.errors.title.message);
+        res.render('login', {message:'Your login or password is incorrect.',css_file:"/base.css"});
       }
     });
-
-  });
-});
-
-
-
-app.post('/addRoutine', function(req,res) {
-  const routine = new Lists({
-    name:req.body.name,
-    workout:[req.body.workout]
-  });
-  routine.save(function(err, doc) {
-    if(!err) {
-      app.get('/' + doc.slug, function(req,res){
-        Lists.find({'slug':doc.slug},function(err,linkObj,count){
-          let thisObj = linkObj[0];
-          console.log(thisObj);
-          res.render('workout',{routine:thisObj,css_file:"/base.css"});
-        });
       });
-      res.redirect('/');
-    } else {
-      res.send(err.errors.title.message);
+
+app.post('/register', function(req, res) {
+  User.findOne({'username':req.body.username},function(err, user) {
+    if (err){
+      console.log('Error in SignUp: '+err);
+      return (err);
     }
+    if (user) {
+        res.render('register',{message:'User Already Exists',css_file:"/base.css"});
+    } else {
+        if(req.body.password.length < 8){
+        console.log('Not long enough');
+        res.render('register',{message:'Password needs to be at least 8 characters long!',css_file:"/base.css"});
+        }
+        else{
+          var newUser = new User();
+          newUser.username = req.body.username;
+          var pw = newUser.generateHash(req.body.password);
+          newUser.password = pw.hash;
+          newUser.pwSalt = pw.salt;
+
+          newUser.save(function(err) {
+            if (err){
+              console.log('Error in Saving user: '+err);
+              throw err;
+            }
+            console.log('User Registration succesful');
+            res.render('index',{css_file:"/base.css",username:req.session.username});
+          });
+      req.session.regenerate((err) => {
+        if (!err) {
+          req.session.username = newUser.username;
+          req.session.password = newUser.password;
+        } else {
+          console.log('error');
+          res.send('an error occurred, please see the server logs for more information');
+        }
+      });
+    }
+    }
+    });
   });
+  app.post('/addRoutine',function(req,res){
+    const lists = new Lists({
+      name: req.body.name,
+      workout: req.body.workout
+    });
+    res.render('routine',{css_file:"/base.css",});
+  });
+
+app.get('/',function(req,res){
+  res.render('home',{css_file:"/base.css"});
+});
+app.get('/home',function(req,res){
+  res.render('index',{css_file:"/base.css"});
+});
+app.get('/addRoutine',function(req,res){
+  res.render('routine',{css_file:"/base.css"});
+});
+app.get('/addWorkout',function(req,res){
+  res.render('workout',{css_file:"/base.css"});
 });
 
-app.listen(3000);
+app.get('/register',function(req,res){
+  res.render('register',{css_file:"/base.css"});
+});
+app.get('/login',function(req,res){
+  res.render('login',{css_file:"/base.css"});
+});
+
+app.listen(process.env.PORT || 3000);
--- db.js
+++ db.js
@@ -3,20 +3,49 @@
 const mongoose = require('mongoose');
 const URLSlugs = require('mongoose-url-slugs');
 
-// schema tells mongoose the types that are allowed in a collection
+ var fs = require('fs');
+ var path = require('path');
+ var fn = path.join(__dirname, 'config.json');
+
+ var bcrypt = require('bcrypt');
+ if (process.env.NODE_ENV === 'PRODUCTION') {
+  // if we're in PRODUCTION mode, then read the configration from a file
+  // use blocking file io to do this...
+  var fs = require('fs');
+  var path = require('path');
+  var fn = path.join(__dirname, 'config.json');
+  var data = fs.readFileSync(fn);
+
+  // our configuration file will be in json, so parse it and set the
+  // conenction string appropriately!
+  var conf = JSON.parse(data);
+  var dbconf = conf.dbconf;
+ } else {
+  // if we're not in PRODUCTION mode, then use
+  dbconf = 'mongodb://localhost/smt430';
+ }
+
 
 var Lists = new mongoose.Schema({
 	name: String,
-	workout: [String]
+	workout: String
 });
 var User = new mongoose.Schema({
   username: String,
-  hash: String,
+  password: String,
+	pwSalt: String,
   lists: [Lists]
 });
+
+User.methods.generateHash = function(password) {
+	console.log(bcrypt);
+    let salt = bcrypt.genSaltSync();
+    console.log(salt);
+    return {salt:salt, hash: bcrypt.hashSync(password, salt, null)};
+};
 
 Lists.plugin(URLSlugs('name'));
 mongoose.model('Lists', Lists);
 mongoose.model('User', User);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/hw05');
+mongoose.connect(dbconf);
