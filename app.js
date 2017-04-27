2bbfc487123c507a37e2e0931f3346f8aad44e9e
sturnbull95 <smt430@nyu.edu>
2017-04-27 14:00:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -153,11 +153,10 @@ app.get('/main.js',function(req,res){
 
 app.post('/api/workout', function(req, res) {
   var bool = {true:true};
-	console.log(req.session.username);
   (new Lists({
       workout: req.body.workout,
       length: req.body.length,
-      user:req.session.username
+      userWork:req.session.username
   })).save(function(err, list, count) {
     res.json(bool);
   });
